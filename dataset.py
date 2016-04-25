45d36207f36dfe0113d2d752c638f7e729191149
Jason Reisman <jasonreisman@gmail.com>
2016-04-24 22:18:26 -0400

    Preprocess dataset for length, hash, and characters, and only load currently batch

--- dataset.py
+++ dataset.py
@@ -1,5 +1,6 @@
 import collections
 import hashlib
+import json
 import numpy as np
 import os
 
@@ -7,59 +8,101 @@ class Dataset(object):
 	_input_path = None
 	_batch_size = None
 	_seq_len = None
-	_hash = None
+	_meta = None
 	_encoding = {}
 	_decoding = {}
-	_encoded_data = None
 	_num_batches = -1
 
 	def __init__(self, input_path, batch_size, seq_len):
 		self._input_path = input_path
 		self._batch_size = batch_size
 		self._seq_len = seq_len
-		self._load_and_create_data()
-		self._create_batches()
-
-	# JR: I don't think that this sorting by frequency stuff makes any difference
-	# since the goal here seems to be that we map chars to some int identifier, 
-	# not that there is any particular structure in the characters themselves.
-	# If there is some advantage to assigning identifiers by frequencies, I don't see it yet
-	def _load_and_create_data(self):
-		input_path = self._input_path
-		assert os.path.exists(input_path), "Input file %s doesn't exist :-(" % (input_path)
-		with open(input_path, 'r') as f:
-			raw_data = f.read()
-		# compute hash of raw data
-		sha1 = hashlib.sha1()
-		sha1.update(raw_data)
-		self._hash = sha1.hexdigest()
-		# get the frequency of each item in raw_data
-		counter = collections.Counter(raw_data)
-		# sort the frequencies in descending order
-		sorted_descending = sorted(counter.items(), key=lambda x : -x[1])
-		# convert pairs in sorted_descending into two lists (chars, freqs)
-		chars, _ = zip(*sorted_descending)
+		# get metadata (length, hash, char set) for the input file
+		# load a cached copy if possible, otherwise create one
+		self._load_or_create_metadata()
+		assert Dataset._metadata_intact(self._meta)
+		# create a dict of char -> int, and int -> char
+		chars = self._meta['chars']
 		num_chars = len(chars)
 		self._encoding = dict(zip(chars, range(num_chars)))
-		# create a dict of int -> char
 		self._decoding = dict(zip(range(num_chars), chars))
-		# convert raw data from chars to symbols
-		self._encoded_data = np.array(list(map(self.encode, raw_data)))
-
-	def _create_batches(self):
-		assert self._encoded_data is not None
 		# figure out how many batches we can actually create
+		data_length = self._meta['length']
 		items_per_batch = self._batch_size*self._seq_len
-		self._num_batches = int(len(self._encoded_data) / items_per_batch) - 1
+		self._num_batches = int(data_length / items_per_batch) - 1
 			# minus 1 to ensure that we have a final symbol in each batch
 			# that matches the first symbol in the next batch
 		assert self._num_batches > 0
 
+	def _load_or_create_metadata(self):
+		meta_path = self._input_path + ".meta"
+		# if metadata file exists, load it
+		if os.path.exists(meta_path):
+			try:
+				with open(meta_path, 'r') as f:
+					meta = json.load(f)
+				if Dataset._metadata_intact(meta):
+					self._meta = meta
+					return
+			except:
+				pass
+		# no metadata file exists, create one
+		self._create_metadata()
+
+	@staticmethod
+	def _metadata_intact(meta):
+		if meta is None:
+			return False
+		if 'length' not in meta:
+			return False
+		if 'hash' not in meta:
+			return False
+		if 'chars' not in meta:
+			return False
+		return True		
+
+	def _create_metadata(self):
+		sha1 = hashlib.sha1()
+		counter = None
+		with open(self._input_path, 'r') as f:
+			# read file in buffer size increments
+			# and compute hash and char frequencies
+			buf = None
+			buf_size = 4096
+			num_chars = 0
+			while buf != "":
+				buf = f.read(buf_size)
+				num_chars += len(buf)
+				sha1.update(buf)
+				c = collections.Counter(buf)
+				counter = counter + c if counter is not None else c
+		# save number of chars in meta
+		self._meta = {}
+		self._meta['length'] = num_chars
+		# save hash in meta
+		self._meta['hash'] = sha1.hexdigest()
+		# Sort the frequencies in descending order
+		# (Strictly speaking this sort isn't necessary, 
+		# since we're only trying to write out the list of characters, but hey)
+		sorted_descending = sorted(counter.items(), key=lambda x : -x[1])
+		# convert pairs in sorted_descending into two lists (chars, freqs)
+		chars, _ = zip(*sorted_descending)
+		self._meta['chars'] = chars
+		# output the meta file
+		with open(meta_path, 'w') as f:
+			json.dump(self._meta, f)
+
 	def get_batches(self):
 		items_per_batch = self._batch_size*self._seq_len
 		for i in xrange(self._num_batches):
-			x0 = self._encoded_data[i*items_per_batch:(i+1)*items_per_batch]
-			y0 = self._encoded_data[i*items_per_batch+1:(i+1)*items_per_batch+1]
+			# read the next batch from the input file
+			with open(self._input_path, 'r') as f:
+				f.seek(i*items_per_batch)
+				raw_data = f.read(items_per_batch + 1)
+				assert len(raw_data) == items_per_batch + 1
+			encoded_data = np.array(list(map(self.encode, raw_data)))
+			x0 = encoded_data[:-1]
+			y0 = encoded_data[1:]
 			x = x0.reshape([self._batch_size, -1])
 			y = y0.reshape([self._batch_size, -1])
 			assert x.shape == (self._batch_size, self._seq_len)
@@ -76,7 +119,7 @@ class Dataset(object):
 
 	@property
 	def hash(self):
-		return self._hash
+		return self._meta['hash']
 
 	@property
 	def num_classes(self):
